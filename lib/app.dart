fe2c648b5636bf1ab6aba33636a9a42253a529b9
stallionbyte <stallionbyte@gmail.com>
2025-05-25 07:45:00 +0000

    Ajout de app.dart

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'core/providers.dart';
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final goRouter = ref.watch(goRouterProvider);
+    return MaterialApp.router(
+      routerConfig: goRouter,
+      theme: ThemeData(primarySwatch: Colors.blue),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,11 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
+import 'app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'core/providers.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ProviderScope(child: MyApp()));
-}
-
-class MyApp extends ConsumerWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final goRouter = ref.watch(goRouterProvider);
-    return MaterialApp.router(
-      routerConfig: goRouter,
-      theme: ThemeData(primarySwatch: Colors.blue),
-    );
-  }
 }
