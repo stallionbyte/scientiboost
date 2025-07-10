df2ff35b86fa6575d2834fb27841f13d4fdab8cb
stallionbyte <stallionbyte@gmail.com>
2025-07-10 22:20:56 +0000

    Revert "feat: Add Chap 11 Exo 2 for PC Nucléaire"

--- lib/features/exos/presentation/screens/pc/nuc/chap11/exo2/enonce.dart
+++ lib/features/exos/presentation/screens/pc/nuc/chap11/exo2/enonce.dart
@@ -1,37 +1,78 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:scientiboost/core/constants.dart';
-
-import 'donnes.dart';
+import 'package:flutter_tex/flutter_tex.dart';
 
 class PcNucChap11Exo2Enonce extends ConsumerWidget {
   const PcNucChap11Exo2Enonce({super.key});
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+
       children: [
-        Text(
-          "Vous travaillez dans un laboratoire d'analyse isotopique et devez distinguer trois isotopes du chlore : chlore-35, chlore-36, et chlore-37.",
-          style: const TextStyle(fontSize: ExoConstants.fontSize),
+        RichText(
+          text: TextSpan(
+            style: TextStyle(fontSize: 18, color: Colors.black),
+            children: <InlineSpan>[
+              const TextSpan(
+                text:
+                    "Après le cours sur le noyau atomique, tu te retrouve avec tes camarades dans un groupe de travail pour traiter des exercices. L'un de tes camarades apporte l'exercice suivant: << l'hélium 4 est un nucléide représenté par le symbole ",
+              ),
+              WidgetSpan(
+                alignment: PlaceholderAlignment.middle,
+                child: TeX2SVG(
+                  math: r"_{4}^{9}He",
+                  formulaWidgetBuilder: (context, svg) {
+                    double displayFontSize = 25;
+                    return SvgPicture.string(
+                      svg,
+                      height: displayFontSize,
+                      width: displayFontSize,
+                      fit: BoxFit.contain,
+                      alignment: Alignment.center,
+                    );
+                  },
+                ),
+              ),
+              const TextSpan(
+                text:
+                    ". Le bérillium 9 est un nucléide représenté par le symbole ",
+              ),
+              WidgetSpan(
+                alignment: PlaceholderAlignment.middle,
+                child: TeX2SVG(
+                  math: r"_{4}^{9}Be",
+                  formulaWidgetBuilder: (context, svg) {
+                    double displayFontSize = 25;
+                    return SvgPicture.string(
+                      svg,
+                      height: displayFontSize,
+                      width: displayFontSize,
+                      fit: BoxFit.contain,
+                      alignment: Alignment.center,
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
-        SizedBox(height: 30),
+
         Text(
-          "1) Établissez un tableau comparatif de la composition nucléaire de ces trois isotopes.",
-          style: const TextStyle(fontSize: ExoConstants.fontSize),
+          '1) Donner une définition du terme nucléide',
+          style: const TextStyle(fontSize: 18, color: Colors.black),
         ),
-        SizedBox(height: 20),
-        Text(
-          "2) Calculez l'énergie de liaison de chlore-35.",
-          style: const TextStyle(fontSize: ExoConstants.fontSize),
+
+        // faire ainsi pour les autre texte
+        Container(
+          alignment: Alignment.centerLeft, // Aligne à gauche
+          // Ajoute un léger espace en haut si nécessaire
+          child: Text(
+            '2) Pour chaque nucléide',
+            style: const TextStyle(fontSize: 18, color: Colors.black),
+          ),
         ),
-        SizedBox(height: 20),
-        Text(
-          "3) Expliquez pourquoi ces trois nucléides appartiennent au même élément chimique malgré leurs propriétés nucléaires différentes.",
-          style: const TextStyle(fontSize: ExoConstants.fontSize),
-        ),
-        SizedBox(height: 20),
-        DonneesExo2Chap11(),
       ],
     );
   }
